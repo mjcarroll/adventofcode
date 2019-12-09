c7894c926ac4ecdbf1823d4774b7eeac2040b840
Michael Carroll <carroll.michael@gmail.com>
2019-12-09 07:13:54 -0600

    2019/9 begin

---
+++ 2019/day09.cc
@@ -0,0 +1,21 @@
+#include "aoc_solution.hh"
+
+SOLUTION(2019, 9);
+
+void AocSolution::test_part1()
+{
+}
+
+std::string AocSolution::part1()
+{
+  return "";
+}
+
+void AocSolution::test_part2()
+{
+}
+
+std::string AocSolution::part2()
+{
+  return "";
+}
